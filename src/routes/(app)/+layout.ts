7dc60e78e61941a23832465d4f11dd84a4fa61b4
João Café <joao.cafe@sky.uk>
2023-04-16 21:38:02 +0100

    fix: SPA settings

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -2,9 +2,6 @@ import { awClient } from '$lib/aw/client';
 import { bucketsStore } from '$lib/stores';
 import { error } from '@sveltejs/kit';
 
-export const prerender = true;
-export const ssr = false;
-
 export async function load() {
 	try {
 		bucketsStore.set(await awClient.getBuckets());
