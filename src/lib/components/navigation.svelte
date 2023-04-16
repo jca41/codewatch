0f6ac9e016b75713702a8c06a5dee1f9290258ee
João Café <joao.cafe@sky.uk>
2023-04-16 01:23:39 +0100

    feat: mesh bg

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -7,8 +7,10 @@
 	];
 </script>
 
-<AppBar>
-	<svelte:fragment slot="lead"><img class="h-6" alt="CodeWatch" src="/logo.png" /></svelte:fragment>
+<AppBar background="bg-surface-700/50" shadow="shadow-lg">
+	<svelte:fragment slot="lead"
+		><img class="h-7 object-contain" alt="CodeWatch" src="/logo.png" /></svelte:fragment
+	>
 	<svelte:fragment slot="trail"
 		><div class="btn-group variant-ghost-secondary">
 			{#each LINKS as link}
